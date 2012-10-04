5d4415d306ae76dc476d872621f237af60921f36
Alex Grachev <alxgrh@yandex.ru>
2012-10-04 13:12:01 +0400

    format code

--- get_cisco_inventory.pl
+++ get_cisco_inventory.pl
@@ -25,115 +25,150 @@
 #  SUCH DAMAGE.
 #
 
-
 use XML::DOM;
 use Net::SNMP;
 
-$version       = "2c";
-$timeout       = 2;
-$oid_sysdescr  = ".1.3.6.1.2.1.1.1.0";
-$oid_sysobjoid = ".1.3.6.1.2.1.1.2";
-$oid_physclass = ".1.3.6.1.2.1.47.1.1.1.1.5";
-$oid_physdescr = ".1.3.6.1.2.1.47.1.1.1.1.2."; 
-$oid_physsn = ".1.3.6.1.2.1.47.1.1.1.1.11.";  
-$oid_physmodelname = ".1.3.6.1.2.1.47.1.1.1.1.13."; 
-$oid_physname = ".1.3.6.1.2.1.47.1.1.1.1.7."; 
+$version           = "2c";
+$timeout           = 2;
+$oid_sysdescr      = ".1.3.6.1.2.1.1.1.0";
+$oid_sysobjoid     = ".1.3.6.1.2.1.1.2";
+$oid_physclass     = ".1.3.6.1.2.1.47.1.1.1.1.5";
+$oid_physdescr     = ".1.3.6.1.2.1.47.1.1.1.1.2.";
+$oid_physsn        = ".1.3.6.1.2.1.47.1.1.1.1.11.";
+$oid_physmodelname = ".1.3.6.1.2.1.47.1.1.1.1.13.";
+$oid_physname      = ".1.3.6.1.2.1.47.1.1.1.1.7.";
 
+my $config;
+if ( $ARGV[0] ) {
+	$config = $ARGV[0];
+}
+else {
+	$config = '/noc/etc/devices.xml';
+}
 
-my $config   = '/noc/etc/devices.xml';
 my $parser = XML::DOM::Parser->new();
 my $doc    = $parser->parsefile($config);
 
 my @array;
 
-
 foreach my $device ( $doc->getElementsByTagName('device') ) {
 	$isactive = $device->getAttribute('active');
-	$role     = $device->getAttribute('role');
-	$name = $device->getElementsByTagName('name')->item(0)->getFirstChild->getData;
-	$address = $device->getElementsByTagName('address')->item(0)->getFirstChild->getData;
+	$name =
+	  $device->getElementsByTagName('name')->item(0)->getFirstChild->getData;
+	$address =
+	  $device->getElementsByTagName('address')->item(0)->getFirstChild->getData;
 
-	$ro_comm = $device->getElementsByTagName('ro_comm')->item(0)->getFirstChild->getData;
-	$dialect = $device->getElementsByTagName('dialect')->item(0)->getFirstChild->getData;
+	$ro_comm =
+	  $device->getElementsByTagName('ro_comm')->item(0)->getFirstChild->getData;
+	$dialect =
+	  $device->getElementsByTagName('dialect')->item(0)->getFirstChild->getData;
 
 	if ( $isactive eq "yes" && $dialect eq "ios12" ) {
-		if ( $role eq "distribution"  || $role eq "access") {
-			print "Device: $name\nRole: $role\n";
-			( $s, $e ) = Net::SNMP->session(
-				-community => $ro_comm,
-				-hostname  => $address,
-				-version   => $version,
-				-timeout   => $timeout,
-			);
-			####check for agent response####
-			if ( !defined( $s->get_request($oid_sysdescr) ) ) {
-				print STDERR "SNMP agent not responding\n";
-				#exit 1;
+		print "Device: $name\n";
+		( $s, $e ) = Net::SNMP->session(
+			-community => $ro_comm,
+			-hostname  => $address,
+			-version   => $version,
+			-timeout   => $timeout,
+		);
+		####check for agent response####
+		if ( !defined( $s->get_request($oid_sysdescr) ) ) {
+			print STDERR "SNMP agent not responding\n";
+
+			#exit 1;
+		}
+		####check for SysOID#########
+		if ( !defined( $s->get_next_request($oid_sysobjoid) ) ) {
+			print STDERR "Cannot define SysObjectID\n";
+		}
+		else {
+			foreach ( $s->var_bind_names() ) {
+				$sysOID = $s->var_bind_list()->{$_};
 			}
-			####check for SysOID#########
-			if (!defined($s->get_next_request($oid_sysobjoid))){
-		    	print STDERR  "Cannot define SysObjectID\n";
-    		}
-    		else {
-    			foreach($s->var_bind_names()) {
-    				$sysOID = $s->var_bind_list()->{$_};
-    			}
-    			$sysobj=`/usr/local/bin/snmptranslate -Ta -m CISCO-PRODUCTS-MIB $sysOID | cut -d : -f 3`;
+			$sysobj =
+			  `snmptranslate -Ta -m CISCO-PRODUCTS-MIB $sysOID | cut -d : -f 3`;
 			chomp $sysobj;
-        		print "SysOID: $sysobj\nInventory:\n";
-    			 
-    		}
-    		######check for class table of physicall entries####
-    		if (!defined($s->get_table($oid_physclass))){
-		    	print STDERR  "Cannot get Physical class table\n";
-    		}
-    		else {
-    			@oid_array=$s->var_bind_names();
-    			$bind_hash = $s->var_bind_list();
-    			foreach (@oid_array) {
-    				$physclass = $bind_hash->{$_};
-    				if($physclass == 10 || $physclass == 9 || $physclass == 3){
-    					$_=~ m/.*\.(\d+)/;
-    					$entityindex=$1;
-    					
-    					######check for physname####
-    					if (!defined($s->get_request($oid_physname . $entityindex))){
-		    				print STDERR "Cannot get Physical name entry\n";
-    					}
-    					else{
-    						$entityname = $s->var_bind_list()->{$oid_physname . $entityindex}; 
-    						print "Name : $entityname\n";
-    					}
-    					######check for physdescr####
-    					if (!defined($s->get_request($oid_physdescr . $entityindex))){
-		    				print STDERR "Cannot get Physical Descr entry\n";
-    					}
-    					else{
-    						$entitydescr = $s->var_bind_list()->{$oid_physdescr . $entityindex}; 
-    						print "Descr : $entitydescr\n";
-    					}
-    					######check for serial number#####
-    					if (!defined($s->get_request($oid_physsn . $entityindex))){
-		    				print STDERR "Cannot get serial num\n";
-    					}
-    					else{
-    						$entitysn = $s->var_bind_list()->{$oid_physsn . $entityindex}; 
-    						print "SN : $entitysn\n";
-    					}
-    					######check for PID#####
-    					if (!defined($s->get_request($oid_physmodelname . $entityindex))){
-		    				print STDERR "Cannot get model name\n";
-    					}
-    					else{
-    						$entitypid = $s->var_bind_list()->{$oid_physmodelname . $entityindex}; 
-    						print "PID : $entitypid\n\n";
-    					}
-    					
-    				}
-    			}
-    		}
-    		
+			print "SysOID: $sysobj\nInventory:\n";
+
+		}
+		######check for class table of physicall entries####
+		if ( !defined( $s->get_table($oid_physclass) ) ) {
+			print STDERR "Cannot get Physical class table\n";
+		}
+		else {
+			@oid_array = $s->var_bind_names();
+			$bind_hash = $s->var_bind_list();
+			foreach (@oid_array) {
+				$physclass = $bind_hash->{$_};
+				if (   $physclass == 10
+					|| $physclass == 9
+					|| $physclass == 3 )
+				{
+					$_ =~ m/.*\.(\d+)/;
+					$entityindex = $1;
+
+					######check for physname####
+					if (
+						!defined(
+							$s->get_request( $oid_physname . $entityindex )
+						)
+					  )
+					{
+						print STDERR "Cannot get Physical name entry\n";
+					}
+					else {
+						$entityname =
+						  $s->var_bind_list()->{ $oid_physname . $entityindex };
+						print "Name : $entityname\n";
+					}
+					######check for physdescr####
+					if (
+						!defined(
+							$s->get_request( $oid_physdescr . $entityindex )
+						)
+					  )
+					{
+						print STDERR "Cannot get Physical Descr entry\n";
+					}
+					else {
+						$entitydescr = $s->var_bind_list()
+						  ->{ $oid_physdescr . $entityindex };
+						print "Descr : $entitydescr\n";
+					}
+					######check for serial number#####
+					if (
+						!defined(
+							$s->get_request( $oid_physsn . $entityindex )
+						)
+					  )
+					{
+						print STDERR "Cannot get serial num\n";
+					}
+					else {
+						$entitysn =
+						  $s->var_bind_list()->{ $oid_physsn . $entityindex };
+						print "SN : $entitysn\n";
+					}
+					######check for PID#####
+					if (
+						!defined(
+							$s->get_request(
+								$oid_physmodelname . $entityindex
+							)
+						)
+					  )
+					{
+						print STDERR "Cannot get model name\n";
+					}
+					else {
+						$entitypid = $s->var_bind_list()
+						  ->{ $oid_physmodelname . $entityindex };
+						print "PID : $entitypid\n\n";
+					}
+
+				}
+			}
 		}
 	}
-		
+
 }
